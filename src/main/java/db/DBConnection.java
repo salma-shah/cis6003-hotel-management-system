6e73cc1c77492c72338ac2abbaaa650759420c2d
Salma Shah <salma.shah.0516@gmail.com>
2026-02-14 09:24:42 +0530

    Aligned the application with Singleton pattern rules

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -11,57 +11,49 @@ public class DBConnection {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/ocean_view_hotel_db?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "admin12345678";
+    private static DBConnection  instance;  // private static field
 
-    private Connection connection;
-
+    // private constructor
     private DBConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            // this is the actual db connection
-            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-            System.out.println("Connected to database successfully");
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (ClassNotFoundException e) {
             System.err.println(e.getMessage());
             throw new RuntimeException("Error connecting to database" + e.getMessage());
         }
     }
 
-    // but this is the INNER static field which holds the single instance
-    private static final DBConnection instance = new DBConnection();
-
     // public method to return the instance
     public static DBConnection getInstance() {
+        if (instance == null) {
+            instance = new DBConnection();
+        }
         return instance;
     }
 
     // method to access the jdbc connection instance
-    public Connection getConnection() {
+    public Connection getConnection() throws SQLException {
         try {
-            // checking if conn is still open
-            if (connection == null || connection.isClosed()) {
-                System.out.println("Connection was close ; re-establishing connection");
-                connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-            }
+            return DriverManager.getConnection(DB_URL, USER, PASSWORD);
         }
-        catch (Exception e) {
-            System.err.println("There was an error: " + e.getMessage());
+        catch (SQLException ex) {
+            throw ex;
         }
-        return connection;
     }
 
-    // closing the connection
-    public void closeConnection() {
-        try
-        {
-            if ( !connection.isClosed() || connection != null)
-            {
-                connection.close();
-                System.out.println("Connection closed successfully");
-            }
-        }
-        catch (Exception e)
-        {
-            System.err.println("There was an error: " + e.getMessage());
-        }
-    }
+//    // closing the connection
+//    public void closeConnection() {
+//        try
+//        {
+//            if ( !connection.isClosed() || connection != null)
+//            {
+//                connection.close();
+//                System.out.println("Connection closed successfully");
+//            }
+//        }
+//        catch (Exception e)
+//        {
+//            System.err.println("There was an error: " + e.getMessage());
+//        }
+//    }
 }
