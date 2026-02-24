461a14481deca770471699dc68061b85d57330c5
Salma Shah <salma.shah.0516@gmail.com>
2026-02-24 20:27:17 +0530

    Implemented change password and sending password changed email

--- src/main/java/mail/impl/PasswordChangeEmail.java
+++ src/main/java/mail/impl/PasswordChangeEmail.java
@@ -2,10 +2,10 @@ package mail.impl;
 
 import mail.EmailBase;
 
-public class PasswordResetEmail implements EmailBase {
+public class PasswordChangeEmail implements EmailBase {
     private final String name, email;
 
-    public PasswordResetEmail(String name, String email) {
+    public PasswordChangeEmail(String name, String email) {
         this.name = name;
         this.email = email;
     }
@@ -17,7 +17,7 @@ public class PasswordResetEmail implements EmailBase {
 
     @Override
     public String getSubject() {
-        return "Reset your Ocean View Resort Password";
+        return "Your Ocean View Resort account password was changed.";
     }
 
     @Override
@@ -26,6 +26,9 @@ public class PasswordResetEmail implements EmailBase {
                 "This is to inform you that the password to your Ocean View Hotel account was just changed.\n" +
                 "If you did not make this change, please immediately inform HR.\n\n" +
                 "Regards," + "\n" +
-                "Ocean View Hotel Team.";
+                "Ocean View Resort Team\n" +
+                "Galle\n" +
+                "Email: ocean.view.hotel.cis6003@gmail.com\n" +
+                "Phone: +94 11 22 33 567";
     }
 }
